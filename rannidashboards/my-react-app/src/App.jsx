7fcab9cbb95dfdb337513ce0c713f1dc468d6c44
Roberto Velarde <betthoow@gmail.com>
2024-06-21 14:59:09 -0700

    React Tutorial - Props management

--- rannidashboards/my-react-app/src/App.jsx
+++ rannidashboards/my-react-app/src/App.jsx
@@ -4,14 +4,26 @@ import Food from "./Food"
 
 import Card from "./Card"
 
+import Button from "./Button"
+
+import Student from "./Student"
+
 function App() {
   return (
       <>
+        <Button></Button>
+        <Student name="Spongebob" age={30} isStudent={true}></Student>
+        <Student name="Leila" age={33} isStudent={false}></Student>
+        <Student name="Rude" age={12} isStudent={false}></Student>
+        <Student name="Sting" age={23} isStudent={true}></Student>
+        <Student ></Student>
         <Header />
+        
         <Card></Card>
         <Card></Card>
         <Card></Card>
         <Card></Card>
+       
         <Food />
         <Footer />
       </>
